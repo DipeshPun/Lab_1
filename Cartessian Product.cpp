8adc6294fa46596c98755a37f50c7c1c024d4cfb
DipeshPun <141342974+DipeshPun@users.noreply.github.com>
2023-08-03 21:27:27 +0545

    Add files via upload

---
+++ Cartessian Product.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[100],b[100],n,i,j;
+    cout<<"Enter the size of set:";
+    cin>>n;
+    cout<<"Enter the elements of set a:"<<endl;
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cout<<"Enter the elements of set b:"<<endl;
+    for(i=0;i<n;i++){
+        cin>>b[i];
+    }
+    cout<<"Cartessian Product=";
+    cout<<"{";
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            cout<<"(";
+            cout<<a[i]<<","<<b[j];
+            cout<<")";
+            cout<<",";
+        }
+    }
+    cout<<"}";
+    return 0;
+}
